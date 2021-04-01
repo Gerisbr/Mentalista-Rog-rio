915787a60a8591830dfc057ff2a0d8fb4e5dad64
geris <73652445+Gerisbr@users.noreply.github.com>
2021-04-02 02:56:02 +0300

    Add files via upload

---
+++ js/funcoes.js
@@ -0,0 +1,8 @@
+let numeroSecreto = 4;
+let chute =parseInt(prompt("digite um número entre 0 e 10:"));
+
+if(chute == numeroSecreto){
+    alert("Parabens, você acertou!");
+}else{
+    alert("Que pena, tente de novo.");
+}
