598be1708968a9fdedb26beb2d6663fccd0410d2
geris <73652445+Gerisbr@users.noreply.github.com>
2021-04-02 02:57:30 +0300

    Update funcoes.js

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,8 +1,18 @@
 let numeroSecreto = 4;
-let chute =parseInt(prompt("digite um número entre 0 e 10:"));
 
-if(chute == numeroSecreto){
-    alert("Parabens, você acertou!");
-}else{
-    alert("Que pena, tente de novo.");
+let tentativas = 3;
+
+while (tentativas > 0) {
+    let chute = parseInt(prompt("Digite um número entre 0 e 10: "));
+
+    if (chute == numeroSecreto) {
+        alert("Parabéns, você acertou!");
+        break;
+    }else if(numeroSecreto > chute){
+        alert("O número secreto é maior que o chute");
+        tentativas = tentativas - 1;
+    }else if(numeroSecreto < chute){
+        alert("O número secreto é menor que o chute");
+        tentativas = tentativas - 1
+    }
 }
